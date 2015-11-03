f4098f997a07325061e6f71e8e5c799580c7cd31
Ike Mattice <gogreen37@me.com>
2015-11-03 14:43:54 -0800

    add navigation and placeholder content.

--- app.js
+++ app.js
@@ -1,16 +1,102 @@
 var ike = angular.module('ike', ['ui.router']);
 
 ike.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('home', {
-        url:'',
+    $stateProvider.state('projects', {
+        url:'/',
         views: {
-            // 'header': {
-            //     templateUrl:'html/header.html',
-            //     controller:'HeaderCtrl'
-            // },
-            'home': {
-                templateUrl:'html/home.html'
+            'header': {
+                templateUrl:'html/header.html',
+                // controller:'HeaderCtrl'
+            },
+            'projects': {
+                templateUrl:'html/projects.html'
+            },
+            'footer': {
+                templateUrl:'html/footer.html'
             },
         }
-    }); //end view state
+    }); //end projects state
+
+    $stateProvider.state('skills', {
+        url:'/skills',
+        views: {
+            'header': {
+                templateUrl:'html/header.html',
+                // controller:'HeaderCtrl'
+            },
+            'skills': {
+                templateUrl:'html/skills.html'
+            },
+            'footer': {
+                templateUrl:'html/footer.html'
+            },
+        }
+    }); //end skills state
+
+    // $stateProvider.state('services', {
+    //     url:'',
+    //     views: {
+    //         'header': {
+    //             templateUrl:'html/header.html',
+    //             // controller:'HeaderCtrl'
+    //         },
+    //         'services': {
+    //             templateUrl:'html/projects.html'
+    //         },
+    // 'footer': {
+    //     templateUrl:'html/footer.html'
+    // },
+    //     }
+    // }); //end projects state
+    //
+    // $stateProvider.state('blog', {
+    //     url:'',
+    //     views: {
+    //         'header': {
+    //             templateUrl:'html/header.html',
+    //             // controller:'HeaderCtrl'
+    //         },
+    //         'blog': {
+    //             templateUrl:'html/projects.html'
+    //         },
+    // 'footer': {
+    //     templateUrl:'html/footer.html'
+    // },
+    //     }
+    // }); //end projects state
+
+    $stateProvider.state('contact', {
+        url:'/contact',
+        views: {
+            'header': {
+                templateUrl:'html/header.html',
+                // controller:'HeaderCtrl'
+            },
+            'contact': {
+                templateUrl:'html/contact.html'
+            },
+            'footer': {
+                templateUrl:'html/footer.html'
+            },
+        }
+    }); //end contact state
+
+    $stateProvider.state('about', {
+        url:'/about',
+        views: {
+            'header': {
+                templateUrl:'html/header.html',
+                // controller:'HeaderCtrl'
+            },
+            'about': {
+                templateUrl:'html/about.html'
+            },
+            'footer': {
+                templateUrl:'html/footer.html'
+            },
+        }
+    }); //end about state
+
+
+
 });  //end config
